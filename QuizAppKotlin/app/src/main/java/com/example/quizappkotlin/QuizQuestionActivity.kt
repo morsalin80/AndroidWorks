4f8cd464bf0a6656a962d6e372fbc8c6cea5fe3c
Md Morsalin Hossain <mdmorsalin80@gmail.com>
2023-05-03 00:59:04 +0600

    Setting up and connecting the question ui
    Signed-off-by: Md Morsalin Hossain <mdmorsalin80@gmail.com>

--- QuizAppKotlin/app/src/main/java/com/example/quizappkotlin/QuizQuestionActivity.kt
+++ QuizAppKotlin/app/src/main/java/com/example/quizappkotlin/QuizQuestionActivity.kt
@@ -1,16 +1,104 @@
 package com.example.quizappkotlin
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
+
+    private var progressBar:ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
+    private var btnSubmit: Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
-        val questionList = Constants.getQuestions()
-        Log.i("QuestionListSize: ",
-            "The Question List size is: ${questionList.size}")
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
+
+        mQuestionList = Constants.getQuestions()
+
+        setQuestion()
+
+    }
+
+    private fun setQuestion() {
+        val question: Question = mQuestionList!![mCurrentPosition - 1]
+
+        ivImage?.setImageResource(question.image)
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.question
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+        if(mCurrentPosition == mQuestionList!!.size) {
+            btnSubmit?.text = "FINISH"
+        } else {
+            btnSubmit?.text = "SUBMIT"
+        }
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+
+        tvOptionOne?.let{
+            options.add(1,it)
+        }
+
+        tvOptionTwo?.let{
+            options.add(1,it)
+        }
+        tvOptionThree?.let{
+            options.add(1,it)
+        }
+        tvOptionFour?.let{
+            options.add(1,it)
+        }
+
+        for(option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
